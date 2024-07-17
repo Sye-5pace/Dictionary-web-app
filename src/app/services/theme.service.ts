3f211f2b0a22cd346b9a8494d0c3f51a34c9af11
Silas Owu <owusilasyp00@gmail.com>
2024-07-17 07:28:38 -0700

    style: persist font service with localstorage

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private theme = new BehaviorSubject<boolean>(false);
+  currentTheme$ = this.theme.asObservable(); 
 
-  constructor() { }
+  constructor() {
+    
+  }
 }
