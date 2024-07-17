d1ae5dcb87aa9388c7b494522fd926def477734e
Silas Owu <owusilasyp00@gmail.com>
2024-07-17 07:24:24 -0700

    style: update header ui

--- src/app/services/font.service.ts
+++ src/app/services/font.service.ts
@@ -7,10 +7,20 @@ import { BehaviorSubject } from 'rxjs'
 
 
 export class FontService {
-  private selectedFont = new BehaviorSubject<string>('Sans Serif');
+  private readonly storageKey = 'currentFont';
+  private selectedFont = new BehaviorSubject<string>(this.readFontStorage());
   currentFont$ = this.selectedFont.asObservable();
 
   changeFont(font: string) {
     this.selectedFont.next(font);
+    this.setFontStorage(font);
+  }
+
+  private setFontStorage(font: string){
+    localStorage.setItem(this.storageKey, font)
+  }
+
+  private readFontStorage() {
+    return localStorage.getItem(this.storageKey) || 'Sans Serif';
   }
 }
