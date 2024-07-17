e1e00782a37eacb268039f10ad2a1274dc0ed74d
Silas Owu <owusilasyp00@gmail.com>
2024-07-17 02:15:21 -0700

    feat: integrate font service into header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,30 @@
 import { Component,  } from '@angular/core';
+import { FontService } from '../../services/font.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 
 
 export class HeaderComponent {
+  selectedFont: string = 'serif'
   fontFamily: string[] = ['Serif', 'Sans', 'Mono'];
-  
+
+  constructor(private fontService: FontService){}
+
+  ngOnInit(){
+    this.fontService.currentFont$.subscribe( font => this.selectedFont = font);
+  }
+
+  onFontChange(font: string){
+    this.fontService.changeFont(font)
+    this.selectedFont = font;
+  }
+
 }
