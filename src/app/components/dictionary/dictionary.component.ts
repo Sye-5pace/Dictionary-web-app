3677b1e8d771c3fe41ce2d735d75596a3085e6c6
Silas Owu <owusilasyp00@gmail.com>
2024-07-17 20:34:43 -0700

    style: update dictionary ui with  visual feedback for error

--- src/app/components/dictionary/dictionary.component.ts
+++ src/app/components/dictionary/dictionary.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { FontService } from '../../services/font.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.css'
 })
 
 export class DictionaryComponent {
   selectedFont$!: string;
-  constructor( private fontService: FontService){}
+  word: string = '';
+  empty: boolean = true;
+
+  constructor( private fontService: FontService, private http: HttpClient){}
 
   ngOnInit(){
     this.fontService.currentFont$.subscribe( font => {
       this.selectedFont$ = font;
     })
+  }
+
+  searchWord(){
+    if(this.word){
+
+    }else{
+      this.empty = true;
+    }
   }
 
 
